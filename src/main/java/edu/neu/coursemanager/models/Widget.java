809e32366cc93a56f3cfa1f5c3062f2aa28369c5
vaibhavshukla <shukla.v@husky.neu.edu>
2018-08-01 23:10:02 -0400

    Added Widget model

---
+++ src/main/java/edu/neu/coursemanager/models/Widget.java
@@ -0,0 +1,110 @@
+package edu.neu.coursemanager.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Widget {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String text;
+	private String widgetType;
+	private int size;
+	private String widgetName;
+	private int widgetOrder;
+	private String imageSrc;
+	private String linkHref;
+	private String listType;
+	
+	@ManyToOne
+	@JsonIgnore
+	private Topic topic;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getWidgetType() {
+		return widgetType;
+	}
+
+	public void setWidgetType(String widgetType) {
+		this.widgetType = widgetType;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public String getWidgetName() {
+		return widgetName;
+	}
+
+	public void setWidgetName(String widgetName) {
+		this.widgetName = widgetName;
+	}
+
+	public int getWidgetOrder() {
+		return widgetOrder;
+	}
+
+	public void setWidgetOrder(int widgetOrder) {
+		this.widgetOrder = widgetOrder;
+	}
+
+	public String getImageSrc() {
+		return imageSrc;
+	}
+
+	public void setImageSrc(String imageSrc) {
+		this.imageSrc = imageSrc;
+	}
+
+	public String getLinkHref() {
+		return linkHref;
+	}
+
+	public void setLinkHref(String linkHref) {
+		this.linkHref = linkHref;
+	}
+
+	public String getListType() {
+		return listType;
+	}
+
+	public void setListType(String listType) {
+		this.listType = listType;
+	}
+
+	public Topic getTOpic() {
+		return topic;
+	}
+
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+
+}
